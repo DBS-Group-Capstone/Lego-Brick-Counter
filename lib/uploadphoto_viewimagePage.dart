e4ec35a32853a867b3b0727e35a7254c291e184a
Brenton Bales <brentonbales@my.unt.edu>
2026-04-18 17:27:46 -0500

    Added dropdown menu and save to gallery functionality to uploadphoto_viewimagePage.dart

--- lib/uploadphoto_viewimagePage.dart
+++ lib/uploadphoto_viewimagePage.dart
@@ -5,8 +5,30 @@ import 'dart:io';
 // import 'package:intl/intl.dart';
 // import 'package:cross_file_image/cross_file_image.dart';
 import 'package:path/path.dart';
+import 'package:gal/gal.dart';
 
+enum MenuItem {deleteImage, saveImage}
 
+// Gives saves the image and returns a boolean determining success
+Future<bool> saveAndVerify(String s) async {
+  try {
+    await Gal.putImage(s);
+  }
+  catch(e) {
+    return false;
+  }
+  return true;
+}
+
+Future<bool> deleteAndVerify(String s) async {
+  try {
+    await File(s).delete();
+  }
+  catch(e) {
+    return false;
+  }
+  return true;
+}
 
 class ViewimagePage extends StatefulWidget {
   const ViewimagePage({super.key});
@@ -38,18 +60,66 @@ class _ViewimagePageState extends State<ViewimagePage> {
         actions: <Widget> [
           IconButton(
             onPressed: ()  {
-              // Delete the file and return
-              File(arg.path).delete();
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.delete_forever)
-          ),
-          IconButton(
-            onPressed: ()  {
               Navigator.of(context).pushNamed('/analyze', arguments:arg);
             },
             icon: Icon(Icons.remove_red_eye)
-          )
+          ),
+          PopupMenuButton<MenuItem>(
+            itemBuilder: (context) => <PopupMenuEntry<MenuItem>> [
+              PopupMenuItem<MenuItem>(
+                value: MenuItem.saveImage,
+                // Save image to gallery
+                child: TextButton(
+                  onPressed: () async {
+                      // Gal.putImage(arg.path);
+                    var did = await saveAndVerify(arg.path);
+                    // Select message based on success
+                    String m ="";
+                    if(did) {
+                      m = "File ${basename(arg.path)} saved to Gallery";
+                    }
+                    else {
+                      m = "File did not save properly";
+                    }
+                    if (!context.mounted) return;
+
+                    // Snackbar message
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text(m)),
+                    );
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Save to Gallery")
+                )
+              ),
+              PopupMenuItem<MenuItem>(
+                value: MenuItem.deleteImage,
+                child: TextButton(
+                  // Delete file and pop
+                  onPressed: () async {
+                    var did = await deleteAndVerify(arg.path);
+                    // Select message based on success
+                    String m ="";
+                    if(did) {
+                      m = "File ${basename(arg.path)} deleted.";
+                    }
+                    else {
+                      m = "File did not delete properly";
+                    }
+                    if (!context.mounted) return;
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text(m)),
+                    );
+
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Delete from app")
+                )
+              )
+            ]
+          ),
         ],
       ),
       body: Center(
