83a0495e95915e4f0b910959038c3e1809bddf3d
Brenton Bales <brentonbales@my.unt.edu>
2026-04-21 23:53:11 -0500

    Added insertBrickAccumulate and insertBrickAccumulateMultiple functions for database_helper.dart

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -30,6 +30,44 @@ class DatabaseHelper {
     return await db.insert('bricks', brick.toMap());
   }
 
+  // Inserts a brick if one with the same fields (other than ID and count) are not there,
+  //   but adds to the count if one with those fields are there
+  Future<int> insertBrickAccumulate(Brick brick) async {
+    final db = await instance.database;
+    var q = await db.query(
+      "bricks",
+      columns: ["id"],
+      where: "color = ? AND size = ? AND type = ?",
+      whereArgs: [brick.color, brick.size, brick.type]
+    );
+    // This sql implementation starts auto index at 1
+    int id = 0;
+    int count = brick.quantity;
+    if(q.isNotEmpty) {
+      brick.id = (q.first["id"] as num).round();
+    }
+    if(id > 0) {
+      count += (q.first["quantity"] as num).round();
+      brick.quantity = count;
+      return await updateBrick(brick);
+    }
+    else if(brick.quantity < 1) {
+      return 0;
+    }
+    else {
+      return insertBrick(brick);
+    }
+  }
+
+  // Same as insertBrickAccumulate but for lists.
+  Future<int> insertBrickAccumulateMultiple(List<Brick> bricks) async {
+    int count = 0;
+    for (var brick in bricks) {
+      count += await insertBrickAccumulate(brick);
+    }
+    return count;
+  }
+
   Future<List<Brick>> getBricks() async {
     final db = await instance.database;
     final result = await db.query('bricks');
